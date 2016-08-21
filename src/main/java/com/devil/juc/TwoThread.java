7200a613655d0d76b61f76e43363f03615874d16
oDevilo <15757166470@163.com>
2016-08-21 23:36:48 +0800

    add two thread

---
+++ src/main/java/com/devil/juc/TwoThread.java
@@ -0,0 +1,65 @@
+package com.devil.juc;
+
+/**
+ * 模仿两个线程交替执行
+ * 
+ * @author Devil
+ * @date 2016年8月21日 下午11:12:30
+ * @version $Id$
+ *
+ */
+public class TwoThread {
+
+	private static Object lock = new Object();
+	private static boolean flag = false;
+
+	public static void main(String[] args) {
+
+		Thread a = new Thread() {
+			public void run() {
+				while (true) {
+					synchronized (lock) {
+						System.out.println("Thread1");
+
+						if (flag) {
+							flag = false;
+							lock.notify();
+							try {
+								lock.wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+
+				}
+			}
+		};
+
+		Thread b = new Thread() {
+			public void run() {
+				while (true) {
+					synchronized (lock) {
+						System.out.println("Thread2");
+
+						if (!flag) {
+							flag = true;
+							try {
+								lock.notify();
+								lock.wait();
+							} catch (Exception e) {
+								// TODO: handle exception
+								e.printStackTrace();
+							}
+						}
+					}
+
+				}
+			}
+		};
+
+		a.start();
+		b.start();
+	}
+}
