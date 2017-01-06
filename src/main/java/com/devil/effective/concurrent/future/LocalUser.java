57f623082bad8c863f9e468b85c87a3a1377278a
oDevilo <15757166470@163.com>
2017-01-06 14:41:56 +0800

    future

--- src/main/java/com/devil/effective/concurrent/future/LocalUser.java
+++ src/main/java/com/devil/effective/concurrent/future/LocalUser.java
@@ -9,18 +9,31 @@ import java.util.concurrent.FutureTask;
  * 客户端
  */
 public class LocalUser {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        // 构建FutureTask
-        FutureTask<String> future = new FutureTask<String>(new Service("a"));
-        ExecutorService executor = Executors.newFixedThreadPool(1);
-        // 执行FutureTask,相当于发送一次请求
-        // 开启线程进行RealData的call()执行
-        executor.submit(future);
-        System.out.println("请求完毕");
-        // 这里可以进行额外的操作，这里使用sleep来代替
-        Thread.sleep(500);
-        System.out.println("Main结束业务");
-        // 获取真正的返回信息，如果此时call方法没有执行完成，则依然等待
-        System.out.println("数据=" + future.get());
-    }
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		// 构建FutureTask
+		FutureTask<String> futurea = new FutureTask<String>(new Service("a", 200));
+		FutureTask<String> futureb = new FutureTask<String>(new Service("b", 30));
+		FutureTask<String> futurec = new FutureTask<String>(new Service("c", 50));
+		FutureTask<String> futured = new FutureTask<String>(new Service("d", 10));
+		FutureTask<String> futuree = new FutureTask<String>(new Service("e", 40));
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		// 执行FutureTask,相当于发送一次请求
+		// 开启线程进行RealData的call()执行
+		executor.submit(futurea);
+		executor.submit(futureb);
+		executor.submit(futurec);
+		executor.submit(futured);
+		executor.submit(futuree);
+		System.out.println("请求完毕");
+		// 这里可以进行额外的操作，这里使用sleep来代替
+		Thread.sleep(500);
+		System.out.println("Main结束业务");
+		// 获取真正的返回信息，如果此时call方法没有执行完成，则依然等待，所以下方按顺序输出
+		System.out.println("数据=" + futureb.get());
+		System.out.println("数据=" + futurec.get());
+		System.out.println("数据=" + futured.get());
+		System.out.println("数据=" + futurea.get());
+		System.out.println("数据=" + futuree.get());
+		executor.shutdown();
+	}
 }
--- src/main/java/com/devil/effective/concurrent/future/Service.java
+++ src/main/java/com/devil/effective/concurrent/future/Service.java
@@ -7,22 +7,23 @@ import java.util.concurrent.Callable;
  */
 public class Service implements Callable<String> {
 
-    private String para;
+	private String para;
+	private int time; // 休眠时间
 
-    public Service(String para) {
-        this.para = para;
-    }
+	public Service(String para, int time) {
+		this.para = para;
+	}
 
-    @Override
-    public String call() throws Exception {
-        // 这里是正真的业务逻辑，需要执行一段时间
-        StringBuffer sBuffer = new StringBuffer();
-        for (int i = 0; i < 10; i++) {
-            sBuffer.append(para);
-            System.out.println("RealData執行：" + i);
-            Thread.sleep(100);
-        }
-        return sBuffer.toString();
-    }
+	@Override
+	public String call() throws Exception {
+		// 这里是正真的业务逻辑，需要执行一段时间
+		StringBuffer sBuffer = new StringBuffer();
+		for (int i = 0; i < 10; i++) {
+			sBuffer.append(para);
+			System.out.println(Thread.currentThread().getName() + "," + para + "執行：" + i);
+			Thread.sleep(100 * time);
+		}
+		return sBuffer.toString();
+	}
 
 }
