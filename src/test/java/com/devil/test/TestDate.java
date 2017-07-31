09ae58aadf4fea66b6a5e607bcdfea0adeef5a9b
yuansheng <yuansheng@raycloud.com>
2017-07-31 14:38:29 +0800

    size

--- src/test/java/com/devil/test/TestDate.java
+++ src/test/java/com/devil/test/TestDate.java
@@ -1,5 +1,8 @@
 package com.devil.test;
 
+import com.devil.utils.SizeOfObject;
+import org.junit.Test;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,5 +10,11 @@ public class TestDate {
 	public static void main(String[] args) {
 		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日HH时");
 		System.out.println(sdf.format(new Date()));
+	}
+
+
+	@Test
+	public void test(){
+		System.out.println(SizeOfObject.sizeOf(new int[Integer.MAX_VALUE / 1024]));
 	}
 }
